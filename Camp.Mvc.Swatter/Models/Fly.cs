072683d8277fa949f3282c82d519d899b9c11ade
Marcel <aureole82@hotmail.com>
2017-02-12 22:59:00 +0100

    Add some validation.

--- Camp.Mvc.Swatter/Models/Fly.cs
+++ Camp.Mvc.Swatter/Models/Fly.cs
@@ -7,14 +7,20 @@ namespace Camp.Mvc.Swatter.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(130)]
+        //[StringLength(130, MinimumLength = 3)]
         [Display(Name = "Title")]
         public string Head { get; set; }
 
         [Display(Name = "Description")]
         public string Body { get; set; }
 
+        [Required]
+        [EmailAddress]
         [Display(Name = "Author")]
-        [DisplayFormat(NullDisplayText= "(unknown)")]
+        [DisplayFormat(NullDisplayText = "(unknown)")]
         public string Creator { get; set; }
 
         [Display(Name = "Created at")]
@@ -22,7 +28,7 @@ namespace Camp.Mvc.Swatter.Models
         public DateTime Born { get; set; } = DateTime.Now;
 
         [Display(Name = "Updated at")]
-        [DisplayFormat(DataFormatString = "{0:d}")]
+        [DisplayFormat(DataFormatString = "{0:F}")]
         public DateTime Updated { get; set; } = DateTime.Now;
 
         [Display(Name = "Severity")]
