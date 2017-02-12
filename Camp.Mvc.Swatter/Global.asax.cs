00558124616d1ae501365f2c5d6d71d150be3059
Marcel <aureole82@hotmail.com>
2017-02-12 13:17:16 +0100

    Add route eception to get Index.htm back.

--- Camp.Mvc.Swatter/Global.asax.cs
+++ Camp.Mvc.Swatter/Global.asax.cs
@@ -9,6 +9,11 @@ namespace Camp.Mvc.Swatter
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            // Allows calling Index.htm if the url doesn't address a controller.
+            RouteTable.Routes.Ignore(
+                url: ""
+            );
+
             RouteTable.Routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
