30f57c102976fd45ed9bd90a79dba2d5985d3492
Marcel <aureole82@hotmail.com>
2017-02-12 13:17:48 +0100

    Add HomeController.

---
+++ Camp.Mvc.Swatter/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Camp.Mvc.Swatter.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
