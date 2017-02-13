a66dceb00af073076cca2b55e68b6165fa12f263
Marcel <aureole82@hotmail.com>
2017-02-13 10:36:50 +0100

    Create a parent model: A pot where all the flies are.

---
+++ Camp.Mvc.Swatter/Models/Pot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Camp.Mvc.Swatter.Models
+{
+    public class Pot
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Abbreviation { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Contact")]
+        public string Chief { get; set; }
+
+        [Display(Name = "Swatting Targets")]
+        public List<Fly> Flies { get; set; } = new List<Fly>();
+    }
+}
